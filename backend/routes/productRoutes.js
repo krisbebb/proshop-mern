016c9fc22209de00bcc20fd1bc4f4b79c3fb8b82
krisbebb <krisbebb@gmail.com>
2020-10-03 13:30:19 +1000

    section 4-24

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -15,7 +15,7 @@ router.get(
   })
 );
 
-// @desc     Fetch singe route
+// @desc     Fetch single route
 // @route    GET /api/products/:id
 // @access   Public
 router.get(
@@ -26,7 +26,8 @@ router.get(
     if (product) {
       res.json(product);
     } else {
-      res.status(404).json({ message: 'Product not found' });
+      res.status(404);
+      throw new Error('Product not found');
     }
   })
 );
