12847e6b731c7e9eef84822d3541850c4e08fb03
krisbebb <krisbebb@gmail.com>
2020-10-09 23:43:42 +1100

    section 10-64

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -12,6 +12,7 @@ import {
   ORDER_LIST_MY_REQUEST,
   ORDER_LIST_MY_SUCCESS,
   ORDER_LIST_MY_FAIL,
+  ORDER_LIST_MY_RESET,
 } from '../constants/orderConstants';
 
 export const orderCreateReducer = (state = { loading: true }, action) => {
@@ -69,6 +70,8 @@ export const orderListMyReducer = (
       return { loading: false, orders: action.payload };
     case ORDER_LIST_MY_FAIL:
       return { loading: false, error: action.payload };
+    case ORDER_LIST_MY_RESET:
+      return { orders: [] };
     default:
       return state;
   }
