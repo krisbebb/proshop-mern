1db5c7e9f5f9d8d133c9096e575582dcdc8222c9
krisbebb <krisbebb@gmail.com>
2020-10-10 12:31:54 +1100

    section 11-66

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -13,6 +13,9 @@ import {
   USER_UPDATE_PROFILE_FAIL,
   USER_UPDATE_PROFILE_REQUEST,
   USER_UPDATE_PROFILE_SUCCESS,
+  USER_LIST_REQUEST,
+  USER_LIST_SUCCESS,
+  USER_LIST_FAIL,
 } from '../constants/userConstants';
 
 export const userLoginReducer = (state = { loading: true }, action) => {
@@ -69,6 +72,22 @@ export const userUpdateProfileReducer = (state = { loading: true }, action) => {
       return { loading: false, success: true, userInfo: action.payload };
 
     case USER_UPDATE_PROFILE_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const userListReducer = (
+  state = { loading: true, users: [] },
+  action
+) => {
+  switch (action.type) {
+    case USER_LIST_REQUEST:
+      return { loading: true };
+    case USER_LIST_SUCCESS:
+      return { loading: false, users: action.payload };
+    case USER_LIST_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
